ad46fdbfa04d0db9d2424050babe75826b32fd32
Aaron Liu <aaronliu0130@gmail.com>
2025-03-16 17:04:37 -0400

    ditto
    evidently i was not feeling well

--- cpplint_unittest.py
+++ cpplint_unittest.py
@@ -7181,7 +7181,6 @@ def run_around_tests(pytestconfig: pytest.Config):
     # obviously we're not going to see all the error categories.  So we
     # only run VerifyAllCategoriesAreSeen() when we don't filter for
     # specific tests.
-    assert pytestconfig.getoption("-k", default=None) in [None, ""]
     if pytestconfig.getoption("-k", default=None) in [None, ""]:
         ErrorCollector(None).VerifyAllCategoriesAreSeen()
 
