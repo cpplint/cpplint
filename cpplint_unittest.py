c0eb131751a00bbaa3d74f9569081f50afa8737d
Aaron Liu <aaronliu0130@gmail.com>
2025-04-05 18:11:34 -0400

    use pytest parametrized instead of parameterized & google todo credit
    This unfortunately means we must use test names like test_samples[silly-includeorder_cfirst] now.
    todo(unknown)s were replaced with the appropriate todo(google)s credit (we do not have the knowledge to be more specific than that)

--- cpplint_unittest.py
+++ cpplint_unittest.py
@@ -30,7 +30,7 @@
 
 """Unit test for cpplint.py."""
 
-# TODO(unknown): Add a good test that tests UpdateIncludeState.
+# TODO(google): Add a good test that tests UpdateIncludeState.
 
 from __future__ import annotations
 
@@ -45,7 +45,6 @@ import sys
 import tempfile
 
 import pytest
-from parameterized import parameterized  # type: ignore[import-untyped]
 
 import cpplint
 
@@ -2232,7 +2231,7 @@ class TestCpplint(CpplintTestBase):
             "const string &turing",
             "const string & godel",
         ]
-        # TODO(unknown): Enable also these tests if and when we ever
+        # TODO(google): Enable also these tests if and when we ever
         # decide to check for arbitrary member references.
         #                         "const Turing & a",
         #                         "const Church& a",
@@ -4606,7 +4605,7 @@ class TestCpplint(CpplintTestBase):
             "",
         )
 
-    @parameterized.expand(["else if", "if", "while", "for", "switch"])
+    @pytest.mark.parametrize("keyword", ["else if", "if", "while", "for", "switch"])
     def testControlClauseWithParensNewline(self, keyword):
         # The % 2 part is pseudorandom whitespace-support testing
         self.TestLintContains(
@@ -4618,7 +4617,7 @@ class TestCpplint(CpplintTestBase):
             f" should be on a separate line  [whitespace/newline] [5]",
         )
 
-    @parameterized.expand(["else", "do", "try"])
+    @pytest.mark.parametrize("keyword", ["else", "do", "try"])
     def testControlClauseWithoutParensNewline(self, keyword):
         # The % 2 part is pseudorandom whitespace-support testing
         self.TestLintContains(
